5c3adce2763b78837012cb300b0c60b08f575924
jose antonio p <120035990@upq.edu.mx>
2024-05-09 02:00:21 -0600

    feat: Improve login and registration forms - Add registration form with two steps: access data and personal information - Implement React Router for navigation between login and registration pages - Install Ant Design for UI components

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -16,7 +16,7 @@ export default function BackgroundBeamsDemo() {
       <form action="" className="flex flex-col justify-center max-w-2xl w-1/3 mx-auto p-4 text-white z-20">
         <h2 className="text-2xl font-bold text-center mb-4">Iniciar sesión</h2>
         <Input
-          placeholder="Nombre de usuario"
+          placeholder="Nombre de usuario / Correo"
           size="large"
           prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
         />
@@ -33,13 +33,9 @@ export default function BackgroundBeamsDemo() {
         <Button type="primary" block className="mt-4 font-bold" size="large">
           Iniciar sesión
         </Button>
-        <p className="text-center mt-2 text-base">O inicia sesión con</p>
-        <div className="flex justify-center mt-2">
-          <Button type="primary" shape="circle" className="mr-2" icon={<GoogleOutlined />} />
-          <Button type="primary" shape="circle" className="mr-2" icon={<XOutlined />} />
-        </div>
+       
       </form>
-      <div className="absolute bottom-4 w-full text-center">
+      <div className="relative mt-5 bottom-4 w-full text-center">
         <p className="text-white">¿Aún no tienes cuenta?</p>
         <Link to="register" className="text-[#1785D6] font-bold hover:text-[#4d82b4]">Regístrate</Link>
       </div>
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,7 +1,159 @@
+import { useState } from 'react';
+import { Steps, Input, Space, Button, InputNumber } from 'antd';
+import { Link } from "react-router-dom"
+
+import {
+  UserOutlined,
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+  LockOutlined,
+  GoogleOutlined,
+  XOutlined,
+  MailOutlined,
+  ArrowRightOutlined
+} from '@ant-design/icons';
+
 export default function Register() {
+
+  const [current, setCurrent] = useState(0);
+
+  const onChange = (value: number) => {
+    console.log('onChange:', value);
+    setCurrent(value);
+  };
+
+  const items = [
+    {
+      title: 'Datos de acceso',
+      description: 'Para poder acceder a la aplicación necesitas un nombre de usuario y contraseña.',
+    },
+    {
+      title: 'Información personal',
+      description: 'Metricas personales para poder calcular diversas métricas de tu cuerpo.',
+    },
+  ];
+
+  //todo, funcion que guarde los datos del primer form
+  const nextStep = () => {
+    setCurrent(current + 1);
+  }
+
+  // todo, funcion que mande los datos del primer y segundo form a la base de datos
+  const handleSubmit = () => {
+    console.log('enviando datos...');
+  }
+
   return (
-    <div>
-      <h2 className="text-white text-xl">Registro:</h2>
+    <div className='w-2/3 md:w-1/3'>
+      <Steps
+        current={current}
+        onChange={onChange}
+        items={items}
+        type='navigation'
+      />
+      {
+        current === 0 ? (
+          <div className='w-full h-auto'>
+            <form className="flex flex-col justify-center mt-4  mx-auto  text-white z-20">
+              <Input
+                placeholder="Nombre"
+                size="large"
+                type='text'
+                prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+              />
+
+              <Input
+                className='mt-4'
+                placeholder="Correo"
+                size="large"
+                type='email'
+                prefix={<MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+              />
+
+              <Space direction="vertical" className="mt-4">
+                <Input.Password
+                  size="large"
+                  placeholder="Ingresa tu contraseña"
+                  prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
+              </Space>
+
+              <Space direction="vertical" className="mt-4">
+                <Input.Password
+                  size="large"
+                  placeholder="Confirma tu contraseña"
+                  prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
+              </Space>
+
+              <Button
+                type="primary"
+                block
+                className="flex justify-center items-center mt-4 font-bold"
+                size="large"
+                onClick={nextStep}
+              >
+                Continuar <ArrowRightOutlined />
+              </Button>
+              
+            </form>
+            <div className="relative mt-5 text-center">
+              <p className="text-white">¿Ya tienes una cuenta?</p>
+              <Link to="/" className="text-[#1785D6] font-bold hover:text-[#4d82b4]">Inicia Sesión</Link>
+            </div>
+          </div>
+        ) : (
+          <div className='w-full h-auto'>
+            <form action="" className="flex flex-col justify-center mt-4 text-white z-20">
+              <label htmlFor="peso" className='text-white text-lg'>Peso</label>
+              <InputNumber
+                className='w-full mt-2'
+                size="large"
+                min={1}
+                max={300}
+                defaultValue={0}
+                name='peso'
+                id='peso'
+              />
+
+              <label htmlFor="altura" className='text-white text-lg mt-4'>Altura (Centimetros)</label>
+              <InputNumber
+                className='w-full mt-2'
+                size="large"
+                min={1}
+                max={300}
+                defaultValue={0}
+                name='altura'
+                id='altura'
+              />
+
+              <label htmlFor="edad" className='text-white text-lg mt-4'>Edad</label>
+              <InputNumber
+                className='w-full mt-2'
+                size="large"
+                min={1}
+                max={300}
+                defaultValue={0}
+                name='edad'
+                id='edad'
+              />
+
+              <Button type="primary" block className="mt-4 font-bold" size="large"
+                onClick={handleSubmit}
+              >
+                Registrarse
+              </Button>
+            </form>
+            <div className="relative mt-5 text-center">
+              <p className="text-white">¿Ya tienes una cuenta?</p>
+              <Link to="/" className="text-[#1785D6] font-bold hover:text-[#4d82b4]">Inicia Sesión</Link>
+            </div>
+          </div>
+        )
+
+      }
     </div>
   )
 }
